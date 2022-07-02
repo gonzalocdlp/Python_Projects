449c220f2aec7256674ebe263f4d440b514f7785
gonzalocdlp <gonzalocdlp@gmail.com>
2022-07-02 16:56:43 -0400

    edited calculator

--- 10calculator/main.py
+++ 10calculator/main.py
@@ -1,6 +1,6 @@
 import art
 
-print(f"{art.logo}\n Welcome to our silent auction")
+print(f"{art.logo}\n Welcome to our Python calculator")
 
 def add(n1, n2):
     return n1+n2
@@ -10,7 +10,7 @@ def multiply(n1,n2):
     return n1*n2
 def divide(n1,n2):
     return n1/n2
-a=int(input('type in the first number'))
+a=float(input('type in the first number'))
 continue_game="y"
 while continue_game=="y":
     operations={
@@ -21,15 +21,16 @@ while continue_game=="y":
     }
     
         
-    b=int(input('type in the second number'))
+    
     for operation in operations:
         print(operation)
     c=input('input one of the operations from above')
+    b=float(input('type in the second number'))
     d=operations[c]
     result=d(a,b)
     print(f"{str(a)} {str(c)} {b} = {str(result)} ")
     a=d(a,b)
-    continue_game = input('end or continue input "y" for yes or n for "no"')
+    continue_game = input('Operate this number again? input "y" to continue or "n" to end')
 
 
 print('goodbye')
