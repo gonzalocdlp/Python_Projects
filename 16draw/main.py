2d372fd7d6559aabbb85f589e70c9f512217a264
gonzalocdlp <gonzalocdlp@gmail.com>
2022-07-09 12:25:32 -0400

    coffeemaker with classes

--- 16draw/main.py
+++ 16draw/main.py
@@ -1,5 +1,28 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
-menucof = Menu.get_items(1)
-print(menucof)
+menu = Menu()
+moneymachine=MoneyMachine()
+coffeemaker=CoffeeMaker()
+continue_game="y"
+while continue_game=="y":
+    choice=input(f"pick {menu.get_items()}").lower()
+    if choice=="off":
+        continue_game="n"
+    elif choice=="report":
+        coffeemaker.report()
+        moneymachine.report()
+    else:
+        drink=menu.find_drink(choice)
+        resources=coffeemaker.is_resource_sufficient(drink)
+        price=drink.cost
+        payment=moneymachine.make_payment(price)
+        if payment==True:
+            if resources==True :
+                coffeemaker.make_coffee(drink)
+            else:
+                print('not enough resources')
+    
+
+
+
