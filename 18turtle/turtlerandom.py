fd8599d60c2e43461f7025824b77872f8c76bc71
gonzalocdlp <gonzalocdlp@gmail.com>
2022-07-10 14:55:39 -0400

    Update turtlerandom.py

--- 18turtle/turtlerandom.py
+++ 18turtle/turtlerandom.py
@@ -1,21 +1,33 @@
 from turtle import Turtle, Screen
 import random
+def randomd():
+    directions=[0,90,180,270]
+    picksides=directions[random.randint(0,3)]
+    return picksides
 def randomt():
     sides= ["left", "right"]
     pickturn=sides[random.randint(0,1)]
     return pickturn
+def randomc():
+    colors= ["blue","red","cyan", "purple", "green", "yellow", "orange"]
+    lengthcolors=len(colors)-1
+    pickcolors=colors[random.randint(0,lengthcolors)]
+    return pickcolors
 tim=Turtle()
 screen= Screen()
-tim.shape('turtle')
 tim.color('blue')
 number=4
-tim.speed(2)
-for i in range(1,100):
+tim.pensize(15)
+tim.speed(9)
+for i in range(1,1000):
     randomt()
+    pickcolors=randomc()
+    tim.color(pickcolors)
+    picksides=randomd()
     if randomt()=='right':
-        tim.right(random.randint(1,180))
-        tim.forward(random.randint(1,40))
+        tim.right(picksides)
+        tim.forward(30)
     if randomt()=='left':
-        tim.left(random.randint(1,180))
-        tim.forward(random.randint(1,40))
+        tim.left(picksides)
+        tim.forward(30)
 
