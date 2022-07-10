1400bd4daf790e518813c82a97f8033ef9f10400
gonzalocdlp <gonzalocdlp@gmail.com>
2022-07-10 13:51:18 -0400

    Create main.py

---
+++ 18turtle/main.py
@@ -0,0 +1,25 @@
+from turtle import Turtle, Screen
+tim=Turtle()
+screen= Screen()
+tim.shape('turtle')
+tim.color('blue')
+for i in range(1,20):
+    tim.penup()
+    tim.forward(10)
+    tim.pendown()
+    tim.forward(10)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
