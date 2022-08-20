99631c58c2e428b890764d132144200dc8a5cdc0
gonzalocdlp <gonzalocdlp@gmail.com>
2022-08-20 12:34:57 -0400

    gitignore and sentient progress

--- 22SentientAnalysis/main.py
+++ 22SentientAnalysis/main.py
@@ -1,5 +1,11 @@
+import os
+import openai
+openai.organization = "org-SnuqljBeSr8VdBfdkdiUOGb8"
+openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.Model.list()
 from newspaper import Article
 from textblob import TextBlob
+from requests import request, Request
 url='https://www.reddit.com/r/webdev/comments/2e0r3y/why_do_we_hate_godaddy/'
 article=Article(url)
 article.download()
